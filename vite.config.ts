d1b06f8155aa6cb06aea210d71a639365f790d2c
pragyansharma <pragyansma@gmail.com>
2025-05-20 12:13:43 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/sk/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
